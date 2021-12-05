77e9174847f7ac57d50c3be1912ab1ca0d05b138
ahmed-torky <ahmedtorky704@gmail.com>
2021-12-06 01:22:50 +0200

    checking the winner.

--- scripts/game.js
+++ scripts/game.js
@@ -1,32 +1,51 @@
 function startNewGame() {
-    if (players[0].name === ''|| players[1].name === ''){
-        alert('please set custom player names for both players!')
-        return;    
-    }
-    activePlayerNameElement.textContent = players[activePlayer].name;
-gameAreaElement.style.display = 'block';
+  if (players[0].name === "" || players[1].name === "") {
+    alert("please set custom player names for both players!");
+    return;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = "block";
 }
+
 function switchPlayer() {
-  if (activePlayer=== 0){
-      activePlayer = 1;
-  }  else{
-      activePlayer = 0;
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
   }
   activePlayerNameElement.textContent = players[activePlayer].name;
 }
 
 function selectGameField(event) {
-    const selectField = event.target;
-    const selectedColumn = selectField.dataset.col - 1;
-    const selectedRow = selectField.dataset.row - 1; 
-if (gameData[selectedRow][selectedColumn] > 0 ){
-    alert('please select an embty field')
+  const selectField = event.target;
+  const selectedColumn = selectField.dataset.col - 1;
+  const selectedRow = selectField.dataset.row - 1;
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert("please select an embty field");
     return;
-}
+  }
 
-    selectField .textContent = players[activePlayer].symbol;
-    selectField .classList.add('disabled');
-gameData[selectedRow][selectedColumn] = activePlayer + 1;
-console.log(gameData);
-switchPlayer();
+  selectField.textContent = players[activePlayer].symbol;
+  selectField.classList.add("disabled");
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+  console.log(gameData);
+  switchPlayer();
+}
+function checkforGameOver() {
+    for (let i = 0; i <3; i++) {
+       if (gameData[i][0] > 0 &&
+     gameData[i][0] === gameData[i][1] && 
+     gameData[i][1] === gameData[i][2]) {
+    return gameData[i][0] ;
+  } 
+    }
+    for (let i = 0; i <3; i++) {
+       if (gameData[0][i] > 0 &&
+     gameData[0][i] === gameData[1][i] && 
+     gameData[1][i] === gameData[2][i]) {
+    return gameData[0][i] ;
+  } 
+    }
+  
+  
 }
