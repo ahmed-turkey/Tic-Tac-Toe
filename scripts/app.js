39a5fe553bf18d072a26d56aa8ee60f98bda9715
ahmed-torky <ahmedtorky704@gmail.com>
2021-12-05 22:43:35 +0200

    adding logic for starting a game

--- scripts/app.js
+++ scripts/app.js
@@ -13,10 +13,12 @@ const playerElementOverLayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop')
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-error');
+const gameAreaElement = document.getElementById('active-game')
+
 const editPlayer1ButtonElement = document.getElementById('edit-player-1-btn');
 const editPlayer2ButtonElement = document.getElementById('edit-player-2-btn');
 const cancelConfigButtonElement = document.getElementById('cancel-config-btn');
-
+const startNewGameButtonElement =document.getElementById('start-game-btn')
 
 editPlayer1ButtonElement.addEventListener('click',openPlayerConfig );
 editPlayer2ButtonElement.addEventListener('click',openPlayerConfig );
@@ -25,3 +27,4 @@ cancelConfigButtonElement.addEventListener('click',closePlayerConfig);
 backdropElement.addEventListener('click',closePlayerConfig);
 
 formElement.addEventListener('submit',savePlayerConfig);
+startNewGameButtonElement.addEventListener('click',startNewGame )
